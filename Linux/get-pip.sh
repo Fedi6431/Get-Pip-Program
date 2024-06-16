196d7ff6ce4ea9bae351c30c20d7135270a93b6a
Fedi6431 <102946457+Fedi6431@users.noreply.github.com>
2024-06-16 22:34:41 +0200

    Update get-pip.sh

--- Linux/get-pip.sh
+++ Linux/get-pip.sh
@@ -1,73 +1,76 @@
 #!/bin/bash
 
-echo -e "What are you using:\n1) Linux\n2) Tremux\n"
-
-read -p "--> " usr_sys
-
-if [[ $usr_sys = 1 ]]; then 
-  apt update
-  apt upgrade
-  apt autoremove -y
-
-  echo "Select an option\n1) install\n2) update\n3) remove\n4) close\n"
-
-  read -p "--> " selectop
-
-  if [[ $selectop = 1 ]]; then 
-    sudo apt install python3-pip -y 
-    echo -e "\n"
-    python3 -m pip install --upgrade pip
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-
-  elif [[ $selectop = 2 ]]; then 
-    pip uninstall pip -y
-    echo -e "\n"
-    sudo apt install python3-pip -y 
-    python3 -m pip install --upgrade pip
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-
-  elif [[ $selectop = 3 ]]; then 
-    pip uninstall pip -y
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-    
-  elif [[ $selectop = 4 ]]; then 
-    exit 0 
+while : 
+do 
+  echo -e "What are you using:\n1) Linux\n2) Tremux\n"
+  
+  read -p "--> " usr_sys
+  
+  if [[ $usr_sys = 1 ]]; then 
+    apt update
+    apt upgrade
+    apt autoremove -y
+  
+    echo -e "Select an option\n1) install\n2) update\n3) remove\n4) close\n"
+  
+    read -p "--> " selectop
+  
+    if [[ $selectop = 1 ]]; then 
+      sudo apt install python3-pip -y 
+      echo -e "\n"
+      python3 -m pip install --upgrade pip
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+  
+    elif [[ $selectop = 2 ]]; then 
+      pip uninstall pip -y
+      echo -e "\n"
+      sudo apt install python3-pip -y 
+      python3 -m pip install --upgrade pip
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+  
+    elif [[ $selectop = 3 ]]; then 
+      pip uninstall pip -y
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+      
+    elif [[ $selectop = 4 ]]; then 
+      exit 0 
+    fi
   fi
-fi
-
-if [[ $usr_sys = 2 ]]; then 
-  pkg update
-  pkg upgrade
-  apt autoremove -y
-
-  echo "Select an option\n1) install\n2) update\n3) remove\n4) close\n"
-
-  read -p "--> " selectop
-
-  if [[ $selectop = 1 ]]; then 
-    pkg install python-pip -y
-    echo -e "\n"
-    python -m pip install --upgrade pip
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-
-  elif [[ $selectop = 2 ]]; then 
-    pkg remove python-pip -y
-    echo -e "\n"
-    pkg install python-pip -y 
-    python -m pip install --upgrade pip
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-
-  elif [[ $selectop = 3 ]]; then 
-    pkg remove python-pip -y
-    echo -e "\n"
-    read -p "Press enter to continue" WAIT
-    
-  elif [[ $selectop = 4 ]]; then 
-    exit 0 
+  
+  if [[ $usr_sys = 2 ]]; then 
+    pkg update
+    pkg upgrade
+    apt autoremove -y
+  
+    echo -e "Select an option\n1) install\n2) update\n3) remove\n4) close\n"
+  
+    read -p "--> " selectop
+  
+    if [[ $selectop = 1 ]]; then 
+      pkg install python-pip -y
+      echo -e "\n"
+      python -m pip install --upgrade pip
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+  
+    elif [[ $selectop = 2 ]]; then 
+      pkg remove python-pip -y
+      echo -e "\n"
+      pkg install python-pip -y 
+      python -m pip install --upgrade pip
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+  
+    elif [[ $selectop = 3 ]]; then 
+      pkg remove python-pip -y
+      echo -e "\n"
+      read -p "Press enter to continue" WAIT
+      
+    elif [[ $selectop = 4 ]]; then 
+      exit 0 
+    fi
   fi
-fi
+done
